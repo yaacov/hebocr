b889ac17b8c0b997c1099f098956bcb97d4f117e
kzamir <kzamir@localhost>
2008-03-29 06:15:29 +0000

    automate po file update

--- libhocr/examples/hocr-gtk/gettext-bootstrap.sh
+++ libhocr/examples/hocr-gtk/gettext-bootstrap.sh
@@ -2,8 +2,12 @@
 
 intltool-extract --type=gettext/glade hocr-gtk.glade
 xgettext --language=Python --keyword=_ --keyword=N_ --output=hocr-gtk.pot hocr-gtk.py hocr-gtk.glade.h
+
 msginit --input=hocr-gtk.pot --locale=he_IL.utf-8
 msginit --input=hocr-gtk.pot --locale=sv_SE.utf-8
+
+msgmerge --update he.po hocr-gtk.pot
+msgmerge --update sv.po hocr-gtk.pot
 
 mkdir he
 mkdir sv
