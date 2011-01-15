ec58c3ba9c42193989a03ec06950e466ea462f82
Diego Iastrubni <diegoiast@gmail.com>
2011-01-15 02:18:50 +0200

    Building out of source is possible again.

--- auto-conf.sh
+++ auto-conf.sh
@@ -7,9 +7,8 @@ autoreconf
 automake --add-missing
 
 mkdir build
-#cd build
-#../configure --prefix=$HOME/hebocr
-./configure --prefix=$HOME/hebocr
+cd build
+../configure --prefix=$HOME/hebocr
 make
 make install
 make dist
